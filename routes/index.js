05b4a2896221e897fff8b0df324281ec81a88a3f
ajrobbins04 <ajrobbins04@gmail.com>
2024-02-05 00:03:51 -0800

    updates

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const contactRoutes = require('./contacts');
 const indexController = require('../controllers/index');
+const swaggerRoutes = require('./swagger');
 
 const router = express.Router();
 
 router.get('/', indexController.promptContactsRoute);
 router.use('/contacts', contactRoutes);
+router.use('/', swaggerRoutes); // mount to be included in apiDocs
 
 module.exports = router;
